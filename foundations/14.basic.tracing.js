ff32edf9945686fd71fd9c14b8f1f19991318cb1
Giannis Papadakis <gpapadakis@gwi.com>
2024-04-16 13:37:26 +0300

    wip

--- foundations/14.basic.tracing.js
+++ foundations/14.basic.tracing.js
@@ -9,6 +9,11 @@ export const options = {
   duration: '1m',
 };
 
+// instrumentHTTP will ensure that all requests made by the http module
+// from this point forward will have a trace context attached.
+//
+// The first argument is a configuration object that
+// can be used to configure the tracer.
 tracing.instrumentHTTP({
   propagator: 'w3c',
 });
@@ -22,6 +27,13 @@ export default function () {
     maxNumberOfToppings: 6,
     minNumberOfToppings: 2
   }
+  // the instrumentHTTP call in the init context replaced
+  // the http module with a version that will automatically
+  // attach a trace context to every request.
+  //
+  // Because the instrumentHTTP call was configured with the
+  // w3c trace propagator, this request will as a result have
+  // a `traceparent` header attached.
   let res = http.post(`${BASE_URL}/api/pizza`, JSON.stringify(restrictions), {
     headers: {
       'Content-Type': 'application/json',
